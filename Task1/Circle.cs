0c68bdb7e75d84ec3fb7926b07b3196800e6df76
Sofia <151773@herzen.spb.ru>
2024-10-11 10:15:23 +0300

    Добавьте файлы проекта.

---
+++ Task1/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal static class Circle
+    {
+        //длина окружности
+        static public double GetLenght(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        //площадь окружности
+        static public double GetSqureCircle(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+        //проверка на принадлежность точки к заданной окружности
+        static public string HasPoint(double r, double x0, double y0, double x1, double y1)
+        {
+            double r2 = Math.Pow(x1 - x0, 2) + Math.Pow(y1 - y0, 2);
+            if (r2 > r * r)
+            {
+                return $"Точка лежит вне окружности, так как значение, полученное через уравнение данной окружности больше квадрата радиуса окружности: {r2} > {r * r}";
+            }
+            else if (r2 == r * r)
+            {
+                return $"Точка лежит на самой окружности, так как значение, полученное через уравнение данной окружности равно квадрату радиуса окружности: {r2} = {r * r}";
+            }
+            else
+            {
+                return $"Точка лежит внутри окружности, так как значение, полученное через уравнение данной окружности меньше квадрата радиуса окружности: {r2} < {r * r}";
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для работы с окружностью!\n\n ");
+            try
+            {
+                Console.WriteLine("Доступные режимы: \n" +
+                    "1. Определение длины окружности по радиусу\n" +
+                    "2. Определение площади круга по радиусу\n" +
+                    "3. Проверка принадлежности точки с заданными координатами кругу с радиусом r и координатами центра");
+                Console.WriteLine("\nВведите номер выбранного режима:");
+                int t = Convert.ToInt32(Console.ReadLine());
+                double r;
+                switch (t)
+                {
+                    case 1:
+                        Console.WriteLine("Введите радиус окружности: ");
+                        r = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Длина окружности с радиусом {0} равна {1}", r, Circle.GetLenght(r));
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите радиус окружности: ");
+                        r = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Площадь круга с радиусом {0} равна {1}", r, Circle.GetSqureCircle(r));
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите радиус окружности: ");
+                        r = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите координату Х центра окружности: ");
+                        double x0 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите координату Y центра окружности: ");
+                        double y0 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите координату Х точки: ");
+                        double x1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите координату Y точки: ");
+                        double y1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(Circle.HasPoint(r, x0, y0, x1, y1));
+                        break;
+                    default:
+                        throw new Exception("Номер данного режима не определен!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! " + ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
